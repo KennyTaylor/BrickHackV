aa55dd296b26b1ffc41ca157ff8038d76c402830
Artem Romanenko <ar5336@g.rit.edu>
2019-02-17 06:43:36 -0800

    yikes

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 const randomNumber = 4; // pseudorandom dice roll 1-6
 const e = 3; // euler's constant
 
@@ -27,15 +29,15 @@ class User{
         this.products = products;
         this.name = name;
     }
-}//boyeee
+}
 // boye, n. non gender-specific
 var boyenames = [
     "Gil Gigliotti",
     "Ben Shapiro",
     "Sherrie Swayne",
-    "Gekyume Onfroy",
+    "Thomas Cotton",
     "Alexander Jones",
-    "Michelle Obama",
+    "Tameka Tom",
     "Youre Mom",
     "Shanti Schwalb",
     "Thelma Teitelbaum",
@@ -43,17 +45,15 @@ var boyenames = [
     "H.P. Lovecraft",
     "Prince Humperdinck",
     "Elongated Muskrat",
-    "Temika Tatom"];
-
-/*
+    "Temika Tatom",
     "Gayla Gammage",
-    "Mork Zuccburger",
+    "Wilfred Westberry",
     "James Jost",
     "Mobius Richard",
     "Geri Gerry",
     "Abigail Averitt"
 ];
-*/
+
 
 
 var bois = [];
@@ -61,27 +61,29 @@ var bois = [];
 for(var i = 0; i < boyenames.length;i++) {
     bois[i] = new User(new Array(), boyenames[i]);
     //insert("string",bois[i].name); // mySQL magic happens here
-}
+    //
+    // insert(bois[i].name); // mySQL magic happens here
 
-bois[0].products = ["WDD1","SVK3","SVK4","SVK11"];// replacing a database access assuming we get the api set up (dont have the api key as of 8:10am sunday
-bois[1].products = ["SVK1","SVK2","WDD10","WDD10"];
+
+}
+bois[0].products = ["SVK1","SVK2"]; // replacing a database access assuming we get the api set up
+bois[1].products = ["WDD1","SVK3","SVK4","SVK11"];
 bois[2].products = ["SVK5","WDD2"];
 bois[3].products = ["SVK6"];
 bois[4].products = ["SVK7","SVK8"];
-bois[5].products = ["SVK9","WDD3","WDD4"];
-bois[6].products = ["SVK10","SVK11"];
-bois[7].products = ["SVK12"];
-bois[8].products = ["SVK13"];
-bois[9].products = ["SVK14"];
-bois[10].products = ["SVK15"];
-bois[11].products = ["SVK16"];
-bois[12].products = ["SVK17"];
-bois[13].products = ["SVK18"];
+bois[5].products = ["SVK9","WDD3"];
+bois[6].products = ["SVK9"];
+bois[7].products = ["SVK10"];
+bois[8].products = ["SVK8"];
+bois[9].products = ["SVK9"];
+bois[10].products = ["SVK10"];
+bois[11].products = ["SVK8"];
+bois[12].products = ["SVK9"];
+bois[13].products = ["SVK10"];
 
 
 function tokensToHumanReadableAlsoDocumentObjectShit(userNum) {
     for (var i = 0; i < bois[userNum].products.length; i++) {
-        console.log(userNum);
         addElement(addElement(addElement(bois[userNum].name,"p"),"div",addElement(bois[userNum].products[i],"p"),"users-data"),"div");
         //addElement(addElement(users[userNum].products[i],"p"),"div");
 
@@ -92,7 +94,7 @@ function scoringBois(userNum) {
     var marijuanaSkore = 0;
     var alcoholScore = "";
     var marijuanaScore = "";
-    for(var i = 0;i < bois[userNum].products.length; i++){
+    for(var i = 0;i < bois[userNum].products.length;i++){
         if(bois[userNum].products[i].substring(0,1) === "W"){
             marijuanaSkore++;
         }
@@ -101,7 +103,7 @@ function scoringBois(userNum) {
         marijuanaScore=marijuanaScore+"🍁";
 
     }
-    for(var i=0;i<bois[userNum].products.length-marijuanaSkore;i++){
+    for(var i=0;i<bois[userNum].products.length;i++){
         alcoholScore=alcoholScore+"🍺";
     }
     addElement(addElement(addElement(bois[userNum].name,"p"),"div",addElement("Alcohol: "+alcoholScore+"  Weed: "+marijuanaScore,"p")),"div");
@@ -111,15 +113,11 @@ function scoringBois(userNum) {
 //stolen (ish) code from mozilla developer thing
 function addElement (content,tag,content2,clasName) {
     // create a new div element
-    addElement(addElement(addElement(bois[userNum].name,"p"),"div",addElement("Alcohol: "+alcoholScore+" Weed: "+marijuanaScore,"p")),"div");
-
-}
-
-
-function addElement (content,tag,content2) {
     var newDiv = document.createElement(tag);
+    // and give it some content
     if("string" === typeof content) {
             var newContent = document.createTextNode(content);
+        // add the text node to the newly created div
         newDiv.appendChild(newContent);
     } else {
         newDiv.appendChild(content);
@@ -132,36 +130,11 @@ function addElement (content,tag,content2) {
             var currentDiv = document.getElementsByClassName("elements");
             currentDiv[0].appendChild(newDiv);
     }
+    // add the newly created element and its content into the DOM
 
     return newDiv;
 }
-var swap = function(arr, a, b) {
-    var temp = arr[a];
-    arr[a] = arr[b];
-    arr[b] = temp;
-};
 
 
-    //for (var i = 0; i < bois.length; i++) {
-/* // failed click listener
-var users = document.getElementsByClassName("users");
-for(var i = 0;i<users.length;i++){
-    users[i].addEventListener('click', function(){console.log("click")}
-        //tokensToHumanReadableAlsoDocumentObjectShit(i)
-    );
-    scoringBois(i);
-    //tokensToHumanReadableAlsoDocumentObjectShit(i);
-}*/
 
-
-/*
-$(function() {
-    var users = document.getElementsByClassName('div.users');
-    $("div.users").each(users,function(index){
-        this.on("click", function(){
-            console.log("Click");
-            tokensToHumanReadableAlsoDocumentObjectShit(index);
-        });
-    });
-});*/
 //parseTokens(boi.products);
